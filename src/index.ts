335433eb50a5ee9e0e92b43079dcddc76e5fecbf
Nikita Izrailev <izrailev@phystech.edu>
2019-08-12 11:36:29 +0300

    athentication with pam on /login url

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express = require('express');
+var pam = require('authenticate-pam');
 const app: express.Application = express();
 
-app.listen(8888);
+app.listen(8888, () => console.log('listening port 8888'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/login', function (req, res) {
+  res.set("Access-Control-Allow-Origin", "*");
+  const password = req.query.password;
+  const login = req.query.login;
+  pam.authenticate(login, password, function(err: any) {
+    if(err) {
+      res.status(401).send(err);
+    }
+    else {
+      res.send(true);
+    }
+  });
 });
 
